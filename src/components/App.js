6f9ad8f2a3f67fd66c7a94e86b1d46651974185b
makh0024 <makh0024@algonquinlive.com>
2022-01-18 00:28:07 -0500

    added bg color change

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Home from "./Home";
 import About from "./About";
 
 import "./App.scss";
 
+import { motion, useViewportScroll, useAnimation } from "framer-motion";
+
 const App = () => {
+  const controls = useAnimation();
+  const { scrollY } = useViewportScroll();
+
+  useEffect(() => {
+    console.log(scrollY.get());
+    scrollY.onChange(() => {
+      if (scrollY.get() < 200) {
+        controls.start("initial");
+      }
+      if (scrollY.get() >= 200) {
+        controls.start("colorChange1");
+      }
+    });
+  }, [scrollY, controls]);
+
+  const colorVariants = {
+    initial: {
+      backgroundColor: "rgb(139, 255, 255)",
+    },
+    colorChange1: {
+      backgroundColor: "rgb(130, 172, 255)",
+    },
+    colorChange2: {
+      backgroundColor: "rgb(143, 113, 255)",
+    },
+    colorChange3: {
+      backgroundColor: "rgb(255, 93, 158)",
+    },
+  };
+
   return (
-    <div>
+    <motion.div
+      initial="initial"
+      animate={controls}
+      variants={colorVariants}
+      id="app"
+    >
       <Home />
       <About />
-    </div>
+    </motion.div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-export let UniversalScrollY = 0;
-window.addEventListener("scroll", () => (UniversalScrollY = window.scrollY));
-
 ReactDOM.render(<App />, document.querySelector("#root"));
